64d764d5c6209ebfa9f3bc6b173768fcaf81059c
fh <fhlqwe@gmail.com>
2024-04-10 12:25:58 +0800

    Add typescript declaration file to package.json

---
+++ index.type.ts
@@ -0,0 +1,12 @@
+declare const _default: {
+  configs: {
+    recommended: {
+      plugins: string[];
+      rules: Record<string, string>;
+    };
+    "flat/recommended": {
+      rules: Record<string, string>;
+    };
+  };
+};
+export default _default;
