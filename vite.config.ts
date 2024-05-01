d4d8097506cd8d3a37ccadd94a703a16d7956763
Vincent Briglia <vincent.briglia@lego.com>
2024-05-01 09:00:03 +0200

    only provide coverage for the files we care about

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import path from 'node:path';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  test: {
+    coverage: {
+      exclude: ['lib'],
+    },
+  },
   build: {
     lib: {
       entry: [path.resolve(import.meta.dirname, 'src/index.ts')],
