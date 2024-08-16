6ae293b5b004ee680b898627dc45e678a3f1db90
BBBoy01 <1156678002@qq.com>
2024-08-16 10:43:28 +0800

    feat: add config name for better debug

--- src/utils.ts
+++ src/utils.ts
@@ -6,8 +6,12 @@ export function createFlatRulesConfig(rulesModule: { [key: string]: unknown }) {
   for (const key of Object.keys(rulesModule)) {
     if (key.endsWith('Rules')) {
       // Ensure the property is a rules set
-      const flatKey = `flat/${kebabCase(key.replace('Rules', ''))}`; // Create the new key
-      flatRulesConfig[flatKey] = { rules: rulesModule[key] }; // Assign the rules to the new key
+      const ruleName = kebabCase(key.replace('Rules', ''));
+      const flatKey = `flat/${ruleName}`; // Create the new key
+      flatRulesConfig[flatKey] = {
+        name: `oxlint/${ruleName}`,
+        rules: rulesModule[key],
+      }; // Assign the rules to the new key
     }
   }
 
