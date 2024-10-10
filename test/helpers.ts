85c56eaa90434469902ba051f2d65f1550915531
Sysix <sysix@sysix-coding.de>
2024-10-10 22:24:39 +0200

    fix(test): fix test for next release and update snapshot

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,9 +1,6 @@
 import type { ESLint } from 'eslint';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __dirname = new URL('.', import.meta.url).pathname;
-const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
+import oxlint from '../src/index.js';
 
 export const ESLintTestConfig: ESLint.Options = {
-  baseConfig: compat.extends('plugin:oxlint/all'),
+  baseConfig: oxlint.configs['flat/all'],
 };
