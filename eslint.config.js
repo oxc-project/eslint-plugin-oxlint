bc0f674915cf9bc500bc56b8abb736dd9fd6cfad
Sysix <sysix@sysix-coding.de>
2024-10-10 23:39:16 +0200

    chore(deps): update typescript eslint to v8.8.1

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,13 @@
 import oxlint from './lib/index.cjs';
 import unicorn from 'eslint-plugin-unicorn';
-import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
-
-const __dirname = new URL('.', import.meta.url).pathname;
-const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
+import tseslint from 'typescript-eslint';
 
 export default [
   eslint.configs.recommended,
   unicorn.configs['flat/recommended'],
-  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  ...tseslint.configs.recommended,
   eslintConfigPrettier,
   oxlint.configs['flat/all'],
   {
