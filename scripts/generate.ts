37039f92072fd94e9a5daaf3fef1c779a8477711
Alexander S. <sysix@sysix-coding.de>
2024-10-27 07:17:14 +0100

    fix(ci): do not bump version on generate, let CI manage it (#199)
    closes #198 
    
    Reason: the generate script already bumped the version in the package
    file. `npm version v0.10.3` did not detect a file change, so it failed.
    
    So we let now the CI manage the package version. Also I made it clearer
    what happens in the CI

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,10 +1,8 @@
-import { writeFileSync } from 'node:fs';
 import { RulesGenerator, RulesGrouping } from './rules-generator.js';
 import { ConfigGenerator } from './config-generator.js';
 import { traverseRules } from './traverse-rules.js';
 import { getLatestVersionFromClonedRepo } from './oxlint-version.js';
 import { TARGET_DIRECTORY, VERSION_PREFIX } from './constants.js';
-import packageJson from '../package.json' with { type: 'json' };
 
 const { successResultArray, failureResultArray } = await traverseRules();
 
@@ -34,11 +32,3 @@ const configGenerator = new ConfigGenerator(oxlintVersion, successResultArray);
   generator.setRulesGrouping(RulesGrouping.CATEGORY);
   await generator.generateRules();
 });
-// Update package.json version
-writeFileSync(
-  '../package.json',
-  JSON.stringify({
-    ...packageJson,
-    version: oxlintVersion.replace(VERSION_PREFIX, '').split('-')[0],
-  })
-);
