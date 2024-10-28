06952e7507ee063532ad55c8ba64003363313a41
Sysix <sysix@sysix-coding.de>
2024-10-28 19:43:24 +0100

    feat: add buildFromOxlintConfig

--- src/build-from-oxlint-config.spec.ts
+++ src/build-from-oxlint-config.spec.ts
@@ -4,6 +4,7 @@ import {
   buildFromOxlintConfigFile,
 } from './build-from-oxlint-config.js';
 import fs from 'node:fs';
+import { execSync } from 'node:child_process';
 
 describe('buildFromOxlintConfig', () => {
   describe('rules values', () => {
@@ -151,5 +152,103 @@ describe('buildFromOxlintConfigFile', () => {
     );
 
     expect(rules).toStrictEqual({});
+  });
+});
+
+const executeOxlintWithConfiguration = (filename: string, content: string) => {
+  fs.writeFileSync(filename, content);
+  let oxlintOutput: string;
+
+  try {
+    // --disabled-<foo>-plugin can be disabled after oxc-project/oxc#6896
+    oxlintOutput = execSync(
+      `npx oxlint -c ${filename} --disable-oxc-plugin --disable-typescript-plugin --disable-unicorn-plugin --disable-react-plugin`,
+      {
+        encoding: 'utf-8',
+      }
+    );
+  } catch {
+    oxlintOutput = '';
+  }
+
+  fs.unlinkSync(filename);
+
+  const result = /with\s(\d+)\srules/.exec(oxlintOutput);
+
+  if (result === null) {
+    return undefined;
+  }
+
+  return parseInt(result[1], 10) ?? undefined;
+};
+
+describe('integration test with oxlint', () => {
+  [
+    // default
+    // {}, can be enabled after oxc-project/oxc#6896
+    // no plugins
+    { plugins: [] },
+    // simple plugin override
+    { plugins: ['vite'] },
+    // custom rule off
+    {
+      plugins: [] /** can be removed after oxc-project/oxc#6896 */,
+      rules: { eqeqeq: 'off' },
+    },
+    // combination plugin + rule
+    { plugins: ['vite'], rules: { eqeqeq: 'off' } },
+    // combination plugin + categires + rules
+    {
+      plugins: ['vite'],
+      categories: { correctness: 'off', style: 'warn' },
+      rules: { eqeqeq: 'off' },
+    },
+    // all categories enabled
+    {
+      plugins: [] /** can be removed after oxc-project/oxc#6896 */,
+      categories: {
+        correctness: 'warn',
+        nursery: 'warn',
+        pedantic: 'warn',
+        perf: 'warn',
+        restriction: 'warn',
+        style: 'warn',
+        suspicious: 'warn',
+      },
+    },
+    // all plugins enabled
+    {
+      plugins: [
+        // can be removed after oxc-project/oxc#6896
+        // 'typescript',
+        // 'unicorn',
+        // 'react',
+        'react-perf',
+        'nextjs',
+        'import',
+        'jsdoc',
+        'jsx-a11y',
+        'n',
+        'promise',
+        'jest',
+        'vitest',
+      ],
+    },
+  ].forEach((json, index) => {
+    const fileContent = JSON.stringify(json);
+
+    it(`should output same rule count for: ${fileContent}`, () => {
+      const oxlintRulesCount = executeOxlintWithConfiguration(
+        `integration-test-${index}-oxlint.json`,
+        fileContent
+      );
+
+      const eslintRules = createConfigFileAndBuildFromIt(
+        `integration-test-${index}-eslint.json`,
+        fileContent
+      );
+
+      expect(Object.keys(eslintRules).length).toBe(oxlintRulesCount);
+    });
   });
 });
