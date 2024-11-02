3a238659c3942c9ae6f578a33750b5a6ca6fb36c
Alexander S. <sysix@sysix-coding.de>
2024-11-03 00:34:00 +0100

    build: use `buildFromOxlintConfigFile` in eslint config (#211)
    will create smaller refactor PRs to resolve the issues

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,16 +1,16 @@
-import oxlint from './src/index.js';
+import { buildFromOxlintConfigFile } from './src/index.js';
 import unicorn from 'eslint-plugin-unicorn';
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
 export default [
+  {
+    ignores: ['dist/'],
+  },
   eslint.configs.recommended,
   unicorn.configs['flat/recommended'],
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
-  oxlint.configs['flat/all'],
-  {
-    ignores: ['dist/'],
-  },
+  ...buildFromOxlintConfigFile('oxlint.json'),
 ];
