c78d9e8a21af958eabb67155e6ddee4960ec2041
Sysix <sysix@sysix-coding.de>
2024-11-29 21:13:22 +0100

    build: renames types files

--- src/build-from-oxlint-config/types.ts
+++ src/build-from-oxlint-config/types.ts
@@ -1,20 +1,8 @@
-import type { Linter } from 'eslint';
-
 export type OxlintConfigPlugins = string[];
 
 export type OxlintConfigCategories = Record<string, unknown>;
 
 export type OxlintConfigRules = Record<string, unknown>;
-
-export type OxlintConfigOverride = {
-  files: string[];
-  plugins?: OxlintConfigPlugins;
-  rules?: OxlintConfigRules;
-};
-
-export type EslintPluginOxLintConfig = Linter.Config<
-  Record<string, 'off' | 'warn'>
->;
 
 export type OxlintConfig = {
   [key: string]: unknown;
