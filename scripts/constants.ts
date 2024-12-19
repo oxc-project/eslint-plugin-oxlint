3ec841f309334346192b53807a066d4e67c01c9f
Sysix <sysix@sysix-coding.de>
2024-12-19 17:20:44 +0100

    build: use `oxlint --rules --format=json` for building rules

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,26 +1,5 @@
-import path from 'node:path';
-import { aliasPluginNames } from '../src/constants.js';
-
-const __dirname = new URL('.', import.meta.url).pathname;
-
-export const TARGET_DIRECTORY = path.resolve(__dirname, '..', '.oxc_sparse');
-export const VERSION_PREFIX = 'oxlint_v';
-export const SPARSE_CLONE_DIRECTORY = 'crates/oxc_linter/src';
-
 // these are the rules that don't have a direct equivalent in the eslint rules
 export const ignoreScope = new Set(['oxc', 'deepscan', 'security']);
 
 // these are the rules that are not fully implemented in oxc
 export const ignoreCategories = new Set(['nursery']);
-
-export function convertScope(scope: string) {
-  return Reflect.has(aliasPluginNames, scope)
-    ? aliasPluginNames[scope as 'eslint']
-    : scope.replace('_', '-');
-}
-
-export function prefixScope(scope: string) {
-  const _scope = convertScope(scope);
-
-  return _scope ? `${_scope}/` : '';
-}
