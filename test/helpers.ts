0d00c4cb17473687cd3ab58fe054d25815af134b
Sysix <sysix@sysix-coding.de>
2025-10-22 19:49:44 +0200

    update

--- test/helpers.ts
+++ test/helpers.ts
@@ -2,7 +2,6 @@ import type { ESLint } from 'eslint';
 import oxlint from '../src/index.js';
 
 export const ESLintTestConfig: ESLint.Options = {
-  // @ts-expect-error -- oxlint.configs['flat/all'] is compatible with `Linter.Config | Linter.Config[]`
   baseConfig: oxlint.configs['flat/all'],
   overrideConfigFile: true,
 };
