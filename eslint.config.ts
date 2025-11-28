35c22d6c67c95a95585304986e4174e149a80a8b
kazuya kawaguchi <kawakazu80@gmail.com>
2025-11-28 23:17:18 +0900

    chore: manage lint ignores in oxlintrc (#566)
    By #564, it sets `ignorePatterns` in `oxlintrc`, meaning it now also
    applies to ESLint.

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,9 +4,6 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-  {
-    ignores: ['dist/'],
-  },
   eslint.configs.recommended,
   unicorn.configs['flat/recommended'],
   ...tseslint.configs.recommended,
